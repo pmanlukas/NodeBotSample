81db8c9efb0e103e3c65ed82783c1e74475a97ee
pmanlukas <lukas.pollmann@tum.de>
2017-12-13 14:16:34 +0100

    add routing and chat connector

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var keys = require('./constants');
+
 var restify = require('restify');
 var builder = require('botbuilder');
 var http = require('http');
@@ -7,3 +9,20 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('listening to %s', server.url);
 })
+
+//connector to connect the bot with the bot framework
+//exchange with own data
+var connector = new builder.ChatConnector({
+    appId: keys.BotKeys.BOTID,
+    appPassword: keys.BotKeys.BOTPASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+
+//route for incoming messages
+server.post('/api/messages', connector.listen());
+
+//default dialog/route for your bot
+bot.dialog('/', function(session, args){
+    session.send("Hi");
+    console.log(session.message.text);
+});
